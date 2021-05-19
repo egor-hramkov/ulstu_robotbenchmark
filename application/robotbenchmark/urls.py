b944c208dc75ff4fa47257a536de1fc0c7fc5832
Vershinin_Dmitriy <d.vershinin@truemachine.ru>
2021-05-19 12:24:10 +0400

    add problem template

--- application/robotbenchmark/urls.py
+++ application/robotbenchmark/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('problem/<str:pk>', views.problem),
 ]
--- application/robotbenchmark/views.py
+++ application/robotbenchmark/views.py
@@ -9,3 +9,10 @@ def home(request):
     problems = Problem.objects.all()
     context = {'problems': problems}
     return render(request, 'robotbenchmark/problemList.html', context)
+
+
+def problem(request, pk):
+    problem = Problem.objects.get(id=pk)
+    initUrl = 'ws://localhost:1999/' + problem.world_path
+    context = {'problem': problem, 'initUrl': initUrl}
+    return render(request, 'robotbenchmark/problem.html', context)
