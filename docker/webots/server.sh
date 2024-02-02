b270828e338744d55d27c6808975205acce2d3a5
egor-hramkov <egor_kuplinov@mail.ru>
2024-02-02 17:09:11 +0400

    fix проблем с билдом

--- docker/webots/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-Xvfb :99 -screen 0 1024x768x16 & 
-python3 webots/resources/web/server/simulation_server.py config/simulation/local.json & 
-python3 webots/resources/web/server/session_server.py config/session/local.json
