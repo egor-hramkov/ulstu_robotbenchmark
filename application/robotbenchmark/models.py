a3e65334c19721233466996ff09c0bc7356b34f9
egor <egor_kuplinov@mail.ru>
2024-04-07 18:19:06 +0400

    Подготовка моделей для соревнования

--- application/robotbenchmark/models.py
+++ application/robotbenchmark/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Problem(models.Model):
-    """Модель соревнования"""
+    """Модель задачи"""
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=1000, null=True)
     world_path = models.CharField(max_length=300)
@@ -17,6 +17,22 @@ class Problem(models.Model):
 
 
 class ProblemUser(models.Model):
+    """Многие ко многим Пользователь-Задача"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    points = models.IntegerField(default=0)
+
+
+class Tournament(models.Model):
+    """Модель Соревнования"""
+    problems = models.ManyToManyField(Problem, related_name='tournaments')
+    users = models.ManyToManyField(User, related_name='user_tournaments')
+    date_start = models.DateTimeField(auto_now_add=True)
+    date_end = models.DateTimeField()
+
+
+class TournamentUser(models.Model):
     """Многие ко многим Пользователь-Соревнование"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
