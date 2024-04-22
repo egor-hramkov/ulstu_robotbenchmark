09ff045da071463c2f353977b523badfb8114336
Егор Храмков <egor_kuplinov@mail.ru>
2024-04-22 20:08:57 +0400

    Рефакторинг урлов

--- application/application/urls.py
+++ application/application/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('robotbenchmark.urls'))
+    path('api/', include('robotbenchmark.urls'))
 ]
--- application/robotbenchmark/urls.py
+++ application/robotbenchmark/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from rest_framework import routers
@@ -16,7 +15,6 @@ from .views.tournament_user_view import TournamentUserViewSet
 from .views.problem_user_view import ProblemUserViewSet
 from .views.problem_view import ProblemViewSet
 from .views.user_view import UserViewSet
-from .views.login_view import LoginView
 
 router = routers.DefaultRouter()
 router.register("users", UserViewSet)
@@ -26,20 +24,17 @@ router.register("users-tournament", TournamentUserViewSet)
 router.register("tournament", TournamentViewSet)
 
 urlpatterns = [
-    path("api/", include(router.urls)),
-    path('api/leaderboard/', LeaderboardView.as_view()),
-    path('api/leaderboard/tournament/<int:tournament_id>/', LeaderboardByTournamentView.as_view()),
-    path('api/leaderboard/problem/<int:problem_id>/', LeaderboardByProblemView.as_view()),
-    path('api/commands/', CommandQueueView.as_view()),
-    path('api/finish/<int:problemuser_id>', FinishProblemView.as_view()),
+    path("", include(router.urls)),
+    path('leaderboard/', LeaderboardView.as_view()),
+    path('leaderboard/tournament/<int:tournament_id>/', LeaderboardByTournamentView.as_view()),
+    path('leaderboard/problem/<int:problem_id>/', LeaderboardByProblemView.as_view()),
+    path('commands/', CommandQueueView.as_view()),
+    path('finish/<int:problemuser_id>', FinishProblemView.as_view()),
 
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
