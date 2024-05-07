f93aa77268952c3c8554af0a4729cbaf327e92e0
Егор Храмков <egor_kuplinov@mail.ru>
2024-05-07 13:55:06 +0400

    более детальная информация о пользователях при получении задачи или турнира

--- application/robotbenchmark/serializers/problem_serializer.py
+++ application/robotbenchmark/serializers/problem_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from robotbenchmark.models import Problem
+from robotbenchmark.serializers.user_serializer import UserSerializer
 
 
 class ProblemSerializer(serializers.ModelSerializer):
     """Сериализатор для модель соревнования"""
+    users = UserSerializer(many=True, read_only=True)
 
     class Meta:
         model = Problem
--- application/robotbenchmark/serializers/tournament_serializer.py
+++ application/robotbenchmark/serializers/tournament_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from robotbenchmark.models import Tournament
+from robotbenchmark.serializers.user_serializer import UserSerializer
 
 
 class TournamentSerializer(serializers.ModelSerializer):
     """Сериализатор для модель соревнования"""
+    users = UserSerializer(many=True, read_only=True)
 
     class Meta:
         model = Tournament
