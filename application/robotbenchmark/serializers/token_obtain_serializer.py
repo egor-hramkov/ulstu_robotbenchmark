7c712722fadc8f5b3488ebd1780f8047dc168d22
Егор Храмков <egor_kuplinov@mail.ru>
2024-05-13 16:05:30 +0400

    Добавление ис супер юсер

--- application/robotbenchmark/serializers/token_obtain_serializer.py
+++ application/robotbenchmark/serializers/token_obtain_serializer.py
@@ -6,10 +6,12 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         # Добавляем айди пользователя в данные токена
+        token['is_superuser'] = user.is_superuser
         token['user_id'] = user.id
         return token
 
     def validate(self, attrs):
         data = super().validate(attrs)
         data['user_id'] = self.user.id
+        data['is_superuser'] = self.user.is_superuser
         return data
