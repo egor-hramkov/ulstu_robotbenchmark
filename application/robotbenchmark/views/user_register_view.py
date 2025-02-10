45c076b41a99af21ada66ebb686788de40f165d3
ghub-ayrtom <molchanovlive@gmail.com>
2025-02-10 20:54:25 +0400

    Add new auth-tokens validation free endpoint for user registration and without is_superuser field

---
+++ application/robotbenchmark/views/user_register_view.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from ..serializers.user_register_serializer import UserRegisterSerializer
+
+
+UserModel = get_user_model()
+
+
+class UserRegisterViewSet(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'user_id': user.id, 'message': 'User successfully registered!'}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
