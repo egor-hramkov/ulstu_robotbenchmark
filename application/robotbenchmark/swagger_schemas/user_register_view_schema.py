b7a4fe78ff31b9cfcd09f9cde279a47d8ca39e34
ghub-ayrtom <molchanovlive@gmail.com>
2025-02-12 21:04:46 +0400

    Edit Swagger schema for UserRegisterViewSet from previous commit

--- application/robotbenchmark/swagger_schemas/user_register_view_schema.py
+++ application/robotbenchmark/swagger_schemas/user_register_view_schema.py
@@ -1,13 +1,25 @@
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework import status
 from ..serializers.user_register_serializer import UserRegisterSerializer
+import serializers
 
 
-user_register_view_schema = extend_schema_view(
-    create=extend_schema(
-        summary="Регистрация нового пользователя", 
-        responses={
-            status.HTTP_200_OK: UserRegisterSerializer, 
-        }
-    ), 
+class UserRegisterResponseSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    message = serializers.CharField()
+
+
+user_register_view_schema = extend_schema(
+    summary="Регистрация нового пользователя", 
+    request=UserRegisterSerializer, 
+    responses={
+        status.HTTP_201_CREATED: OpenApiResponse(
+            response=UserRegisterResponseSerializer, 
+            description="Успешная регистрация пользователя", 
+        ), 
+        status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+            response=UserRegisterSerializer, 
+            description="Ошибка валидации", 
+        ), 
+    }
 )
