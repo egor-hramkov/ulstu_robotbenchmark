22a37c32ff05da89707a0cbcbd9bc363271d264b
egor <egor_kuplinov@mail.ru>
2025-02-19 17:51:13 +0400

    Добавлено получение списка записей пользователя

--- application/robotbenchmark/serializers/problem_user_serializer.py
+++ application/robotbenchmark/serializers/problem_user_serializer.py
@@ -1,8 +1,13 @@
+import os
+from django.conf import settings
+
 from rest_framework import serializers
 from robotbenchmark.models import ProblemUser
 
 class ProblemUserSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Соревнование-Пользователь (многие ко многим)"""
+
+    records = serializers.SerializerMethodField()
 
     class Meta:
         model = ProblemUser
@@ -14,3 +19,23 @@ class ProblemUserSerializer(serializers.ModelSerializer):
             'vs_port': {'read_only': True}, 
             'webots_stream_port': {'read_only': True}, 
         }
+
+    def get_records(self, obj):
+        """Получает записи из media/records/<user_name>/records/"""
+
+        # Формируем путь для конкретного пользователя
+        user_name = obj.user.username + str(obj.id)
+        directory = os.path.join(settings.MEDIA_ROOT, 'records', user_name, 'records')
+        records_dict = {}
+
+        # Проверяем, существует ли директория
+        if os.path.exists(directory):
+            # Перебираем все подпапки внутри этой директории
+            for entry in os.listdir(directory):
+                full_path = os.path.join(directory, entry, 'webots_animation.html')
+                if os.path.isfile(full_path):  # Проверяем, чтобы файл существовал
+                    # Добавляем запись в словарь: ключ — время (название папки), значение — путь
+                    records_dict[entry] = os.path.join(settings.MEDIA_URL, 'records', user_name, 'records', entry,
+                                                       'webots_animation.html')
+
+        return records_dict
