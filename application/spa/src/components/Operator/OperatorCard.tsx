7dfb42430cc1aa78ccabf5c0612f3a98aa4b8a9e
timdrake777 <dbondarenko.work@yandex.ru>
2025-02-19 20:21:56 +0400

    Fix фронта

--- application/spa/src/components/Operator/OperatorCard.tsx
+++ application/spa/src/components/Operator/OperatorCard.tsx
@@ -173,9 +173,6 @@ export const OperatorCard = () => {
             Команда для запуска:{" "}
             <strong>{currentProblem?.launch_command ?? '-'}</strong>
           </Text>
-          <Text style={{ display: "block", marginBottom: 10}}>
-            <strong>{currentProblem?.records ?? '-'}</strong>
-          </Text>
             {loading ? (
               <Spin size="large" style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }} />
             ) : (
@@ -194,6 +191,22 @@ export const OperatorCard = () => {
             <Button disabled={!currentProblem?.id} type="primary" onClick={() => launchUserProblem(currentProblem?.id)}>Запустить решение</Button>
             <Button icon={<RightOutlined />} onClick={nextProblem} />
           </Row>
+          
+          <Card title="Записи проездов">
+          <List
+              bordered
+              dataSource={currentProblem && Object.entries(currentProblem?.records)}
+              renderItem={(problem, index) => {
+                return (
+                  <List.Item
+                    key={index}
+                  >
+                    {`${problem[0]} - ${<a href={`https://virtual.robocross.ru:444` + problem[1]}></a>}`}
+                  </List.Item>
+                );
+              }}
+            />
+          </Card>
         </div>
       </Col>
 
